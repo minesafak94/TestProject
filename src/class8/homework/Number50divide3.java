eb73e85dbd372b9ded480a3b6c1b89205afcb28e
minesafak94 <minesafak94@gmail.com>
2023-01-15 10:03:37 -0500

    Homework8

---
+++ src/class8/homework/Number50divide3.java
@@ -0,0 +1,13 @@
+package class8.homework;
+
+public class Number50divide3 {
+    public static void main(String[] args) {
+        //1) Print numbers from 1 to 50 except those that are divisible by 3
+
+        for (int number=1;number<50;number++){
+            if (number%3==0){
+            continue;}
+            System.out.println(number);
+}
+        }
+    }
---
+++ src/class8/homework/PriceItem.java
@@ -0,0 +1,13 @@
+package class8.homework;
+
+public class PriceItem {
+    public static void main(String[] args) {
+
+       /*4) Write a program to ask a user to enter item
+        they want to buy and the price of that item.
+        Every time user enters money accumulate the amount and
+        tell the user how much is left to pay off. If user
+        give more money program should return a change.
+        Whenever a user done with payments program should say "Thank you for shopping!"*/
+    }
+}
